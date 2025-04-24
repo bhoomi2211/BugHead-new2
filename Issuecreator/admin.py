dbc1059b4e4847ab28b4b8fb8faa0585e2cb44be
bhoomi2211 <bhoomiagarawal2211@gmail.com>
2025-04-24 16:13:28 +0530

    fixed everything

--- Issuecreator/admin.py
+++ Issuecreator/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from.models import Issue
+from.models import Website
 
-admin.site.register(Issue)
+admin.site.register(Website)
--- Issuecreator/models.py
+++ Issuecreator/models.py
@@ -2,15 +2,12 @@ from django.db import models
 
 # Create your models here.
   
-class Issue(models.Model):
+class Website(models.Model):
     owner= models.CharField(max_length = 150)
     websitelink= models.CharField(max_length=20)
-    gitHubRepo= models.TextField()
+    gitHubRepo= models.CharField(max_length=50)
     create_at= models.DateTimeField(auto_now=True)
-    update_at=models.DateTimeField(auto_now_add= True)
-    feedback= models.TextField()
-    
+    update_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-        
+        return self.owner
