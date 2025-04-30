acd6c9024eee30b43515b119659cb084e7e751f8
bhoomi2211 <bhoomiagarawal2211@gmail.com>
2025-04-30 14:59:55 +0530

    setup

--- Issuecreator/admin.py
+++ Issuecreator/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from.models import Website
+from.models import Issue
 
 admin.site.register(Website)
+admin.site.register(Issue)
--- Issuecreator/models.py
+++ Issuecreator/models.py
@@ -11,3 +11,17 @@ class Website(models.Model):
 
     def __str__(self):
         return self.owner
+    
+class Issue(models.Model):
+      bugArea= models.CharField(max_length=200)   
+      priority= models.TextField()
+      IssueDetail= models.CharField(max_length = 150)
+      Device= models.CharField(max_length=50)
+      Browse= models.CharField(max_length=100)
+      OperatingSystem= models.CharField(max_length=100)
+
+      def __str__(self):
+         return self.bugArea
+       
+      
+      
